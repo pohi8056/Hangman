002b7a5602562610c73ef5462a57cbd434694755
gurkstav <gustav.dyrssen@live.se>
2015-11-17 18:59:41 +0100

    HangmanClient.java

---
+++ HangmanClient.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.ConnectException;
+import java.rmi.Naming;
+
+
+public class HangmanClient {
+
+    private Hangman hangman;
+
+    private BufferedReader in;
+
+    public String getUniqueLetters(String word) {
+
+	String empty = "";
+
+	for (int i = 0; i < word.length(); i++) {
+	    char character = word.charAt(i);
+	    
+	}
+    }
+
+
+}
