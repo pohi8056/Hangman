5894019fc15d2d3bf64b5ce8656c026402254b33
gurkstav <gustav.dyrssen@live.se>
2015-12-06 17:14:39 +0100

    filer

---
+++ tempTest.java
@@ -0,0 +1,14 @@
+public class tempTest{
+
+    public static void main (String[] args){
+	
+	char[] testArray = {'a', 'b'};
+ 
+	Hangman testHangman = new Hangman('a', 10, 4, "ICE", testArray, 'I');
+	
+
+	testHangman.printAll();
+
+    }
+
+}
